def811c883138f74ad66041b8c1d40b1f6f81e8a
Kobus Burger <kobusgburger@gmail.com>
2018-07-18 21:44:55 +0200

    Export functions are running but PricedBills is very slow

--- BillMacros/MExportBills.vb
+++ BillMacros/MExportBills.vb
@@ -43,14 +43,15 @@
         XlSh = XlWb.ActiveSheet
         FName = Left(XlWb.Name, (InStrRev(XlWb.Name, ".", -1, vbTextCompare) - 1))
         If Not xlAp.Dialogs(Excel.XlBuiltInDialog.xlDialogSaveAs).Show(FName & " Stripped") Then Exit Sub
-
         For Each Wksht In XlWb.Worksheets
-            Wksht.Visible = Excel.XlSheetVisibility.xlSheetVisible 'Worksheets must be visible to avoid errors
+            xlAp.StatusBar = "Sheet: " & Wksht.Name
+            xlAp.ScreenUpdating = False
+            '            Wksht.Visible = Excel.XlSheetVisibility.xlSheetVisible 'Worksheets must be visible to avoid errors
             Select Case Wksht.Tab.Color
-                Case Drawing.Color.Red 'Red = BillSheet
+                Case Excel.XlRgbColor.rgbRed 'Red = BillSheet
                     Wksht.UsedRange.Value = Wksht.UsedRange.Value 'Remove formulas
                     DeleteXtraRowsCols(Wksht, "#BillEnd#", AmtCol)
-                Case Drawing.Color.Green 'Green = Summary
+                Case Excel.XlRgbColor.rgbGreen 'Green = Summary
                     Wksht.UsedRange.Value = Wksht.UsedRange.Value 'Remove formulas
                     DeleteXtraRowsCols(Wksht, "#SumEnd#", SumAmtCol)
                 Case Else
@@ -58,8 +59,9 @@
                     Wksht.Delete()
                     xlAp.DisplayAlerts = True
             End Select
+            xlAp.ScreenUpdating = True
         Next
-
+        xlAp.StatusBar = False
     End Sub
     Sub CreatePriced()
         'Delete hidden rows, delete non-bill columns, delete non-bill sheets & copy priced columns to bill
@@ -74,18 +76,19 @@
         'Save bill with new name
         FName = Left(XlWb.Name, (InStrRev(XlWb.Name, ".", -1, vbTextCompare) - 1))
         If Not xlAp.Dialogs(Excel.XlBuiltInDialog.xlDialogSaveAs).Show(FName & " Priced") Then Exit Sub
-
         MaxRowNo = xlAp.Rows.Count
         MaxColNo = xlAp.Columns.Count
         For Each Wksht In XlWb.Worksheets
-            Wksht.Visible = Excel.XlSheetVisibility.xlSheetVisible 'Worksheets must be visible to avoid errors
+            xlAp.StatusBar = "Sheet: " & Wksht.Name
+            xlAp.ScreenUpdating = False
+            '           Wksht.Visible = Excel.XlSheetVisibility.xlSheetVisible 'Worksheets must be visible to avoid errors
             Select Case Wksht.Tab.Color
-                Case Drawing.Color.Red 'Red = BillSheet
+                Case Excel.XlRgbColor.rgbRed 'Red = BillSheet
                     Wksht.UsedRange.Value = Wksht.UsedRange.Value 'Remove formulas
                     Wksht.Columns(PricedAmtCol).Copy(Wksht.Columns(AmtCol))
-                    Wksht.Columns(PricedRateCol).Copy(Wksht.Columns(RateCol))
+                    Wksht.Columns(PricedRateCol).Copy(Wksht.Columns(RateCol)) 'todo This is very slow
                     DeleteXtraRowsCols(Wksht, "#BillEnd#", AmtCol)
-                Case Drawing.Color.Green 'Green = Summary
+                Case Excel.XlRgbColor.rgbGreen 'Green = Summary
                     Wksht.UsedRange.Value = Wksht.UsedRange.Value 'Remove formulas
                     Wksht.Columns(SumPricedAmtCol).Copy(Wksht.Columns(SumAmtCol))
                     DeleteXtraRowsCols(Wksht, "#SumEnd#", SumAmtCol)
@@ -94,27 +97,24 @@
                     Wksht.Delete()
                     xlAp.DisplayAlerts = True
             End Select
+            xlAp.ScreenUpdating = True
         Next
-
+        xlAp.StatusBar = False
     End Sub
     Sub DeleteXtraRowsCols(Wksht As Excel.Worksheet, EndTxt As String, LastUsedCol As Integer)
         'Delete column A, delete rows below last used row, delete colums right of LastUsedCol & delete hidden rows
         Dim MaxRowNo As Long, LastUsedRow As Long
         Dim MaxColNo As Long, RowNo As Long, TotRows As Long
         Dim xlAp As Excel.Application
-        Dim XlWb As Excel.Workbook
-        Dim XlSh As Excel.Worksheet
         xlAp = Globals.ThisAddIn.Application
-        XlWb = xlAp.ActiveWorkbook
-        XlSh = XlWb.ActiveSheet
         MaxRowNo = Wksht.Rows.Count
         MaxColNo = Wksht.Columns.Count
         Wksht.Select()
 
         If Not Wksht.Cells.Find(EndTxt, SearchOrder:=Excel.XlSearchOrder.xlByRows, SearchDirection:=Excel.XlSearchDirection.xlPrevious) Is Nothing Then
             LastUsedRow = Wksht.Cells.Find(EndTxt, SearchOrder:=Excel.XlSearchOrder.xlByRows, SearchDirection:=Excel.XlSearchDirection.xlPrevious).Row
-            Wksht.Range(XlSh.Cells(LastUsedRow + 1, 1), XlSh.Cells(MaxRowNo, 1)).EntireRow.Delete()
-            Wksht.Range(XlSh.Cells(1, LastUsedCol + 1), XlSh.Cells(1, MaxColNo)).EntireColumn.Delete()
+            Wksht.Range(Wksht.Cells(LastUsedRow + 1, 1), Wksht.Cells(MaxRowNo, 1)).EntireRow.Delete()
+            Wksht.Range(Wksht.Cells(1, LastUsedCol + 1), Wksht.Cells(1, MaxColNo)).EntireColumn.Delete()
             Wksht.Columns("A:A").Delete
             TotRows = Wksht.UsedRange.Rows.Count
             For RowNo = 1 To TotRows
